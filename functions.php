34ce044964e3f6c45b5fb5988064f2d0bc399fa3
Stephan Wagner <stephanwagner.me@gmail.com>
2021-09-07 15:57:26 +0100

    Add header and footer styles

--- functions.php
+++ functions.php
@@ -98,7 +98,7 @@ function html5_search_form()
 	$form = '
 		<form role="search" method="get" action="' . home_url('/') . '" >
 			<input type="text" value="' . get_search_query() . '" class="search__input" name="s" placeholder="Search...">
-			<button type="submit" class="search__button">Search</button>
+			<button type="submit" class="button search__button">Search</button>
 		</form>
 	';
 
