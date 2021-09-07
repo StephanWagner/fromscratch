6cee3e0e2f76b1e999c0ba412c236bee2d8261fd
Stephan Wagner <stephanwagner.me@gmail.com>
2021-09-07 17:43:07 +0100

    Add scroll events

--- src/js/main.js
+++ src/js/main.js
@@ -5,79 +5,28 @@ var app = {};
 // Document is ready
 
 $(function () {
-
   // Append scroll check events
   $(window).on('resize', function () {
-    checkScrollToTop();
     checkScroll();
   });
-  checkScrollToTop();
 
   $(window).on('scroll', function () {
     checkScroll();
   });
   checkScroll();
-
 });
-
-// Scrolling
-
-function scrollToTop() {
-  $('html, body').animate({ scrollTop: 0 }, 800);
-}
-
-function scrollToContentElement(targetElement, duration) {
-  var top = targetElement.offset().top;
-  var scrollTop = getScrollTopWithOffset(top);
-  $('html, body').animate(
-    { scrollTop: scrollTop },
-    {
-      duration: duration || 800,
-      complete: function () {
-        if (targetElement.offset().top != top) {
-          $('html, body').scrollTop(
-            getScrollTopWithOffset(targetElement.offset().top)
-          );
-        }
-      }
-    }
-  );
-}
-
-function getScrollTopWithOffset(top) {
-  var anchorLinkOffset = 72 + 8;
-  if ($(window).width() <= 600) {
-    anchorLinkOffset = 60 + 8;
-  }
-  return top - anchorLinkOffset;
-}
-
-// Check for scrolling availability
-
-function checkScrollToTop() {
-  if ($('body').height() - 120 <= $(window).height()) {
-    $('body').addClass('no-scroll');
-  } else {
-    $('body').removeClass('no-scroll');
-  }
-}
 
 // Check for scrolling position
 
 function checkScroll() {
-  var targetElement = $('.main-menu__placeholder');
+  var activateMinimizedNav = 120;
 
-  if (!targetElement.length) {
+  if ($(window).scrollTop() > activateMinimizedNav) {
+    $('body').addClass('nav-fixed');
     return;
   }
 
-  var top = targetElement.offset().top;
-
-  if ($(window).scrollTop() >= top) {
-    $('body').addClass('nav-fixed');
-  } else {
-    $('body').removeClass('nav-fixed');
-  }
+  $('body').removeClass('nav-fixed');
 }
 
 // Toggle menu
