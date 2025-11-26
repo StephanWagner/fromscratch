be370c8d999493972a902688212c93d5f8363142
Stephan Wagner <stephanwagner.me@gmail.com>
2025-11-26 19:54:41 +0930

    Add anchor link block

--- themes/fromscratch/blocks/anchor-link.php
+++ themes/fromscratch/blocks/anchor-link.php
@@ -3,10 +3,10 @@
 $id = block_field('id');
 ?>
 
-<div class="ecb__blank-block-template">
-
-|| ID: <?php echo $id; ?> ||
-
-    <b>This is the template file for the block &quot;Anker Link&quot;.</b>
-    <p>It is located at <code>/blocks/anchor-link.php</code> in your theme folder.</p>
-</div>
+<?php if (is_admin()) { ?>
+    <div class="block-preview__wrapper">
+        <b>Anker Link:</b> <code><?= $id ?></code>
+    </div>
+<?php } else { ?>
+    <div data-anchor="<?= $id ?>"></div>
+<?php } ?>
