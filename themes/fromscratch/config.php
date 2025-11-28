ea5c65b8eb2adcd8ec70dcdfc59010328f9e362f
Stephan Wagner <stephanwagner.me@gmail.com>
2025-11-28 10:37:34 +0930

    Refactor scss

--- themes/fromscratch/config.php
+++ themes/fromscratch/config.php
@@ -32,12 +32,12 @@ return [
 		['slug' => 'secondary', 'color' => '#00ddff', 'name' => 'Sekundärfarbe'],
 
 		// Grayscale
-		['slug' => 'gray-1', 'color' => '#666', 'name' => 'Grau 1'],
-		['slug' => 'gray-2', 'color' => '#999', 'name' => 'Grau 2'],
-		['slug' => 'gray-3', 'color' => '#ccc', 'name' => 'Grau 3'],
-		['slug' => 'gray-4', 'color' => '#ddd', 'name' => 'Grau 4'],
-		['slug' => 'gray-5', 'color' => '#eee', 'name' => 'Grau 5'],
-		['slug' => 'gray-6', 'color' => '#fafafa', 'name' => 'Grau 6'],
+		['slug' => 'gray-600', 'color' => '#666', 'name' => 'Grau 600'],
+		['slug' => 'gray-500', 'color' => '#999', 'name' => 'Grau 500'],
+		['slug' => 'gray-400', 'color' => '#ccc', 'name' => 'Grau 400'],
+		['slug' => 'gray-300', 'color' => '#ddd', 'name' => 'Grau 300'],
+		['slug' => 'gray-200', 'color' => '#eee', 'name' => 'Grau 200'],
+		['slug' => 'gray-100', 'color' => '#fafafa', 'name' => 'Grau 100'],
 
 		// Status colors
 		['slug' => 'error', 'color' => '#f33', 'name' => 'Status: Fehler'],
@@ -48,25 +48,25 @@ return [
 	// Font sizes
 	'theme_font_sizes' => [
 		[
-			'name' => 'Small',
+			'name' => 'Klein',
 			'shortName' => 'S',
 			'size' => 14,
 			'slug' => 's',
 		],
 		[
-			'name' => 'Regular',
+			'name' => 'Normal',
 			'shortName' => 'M',
 			'size' => 16,
 			'slug' => 'm',
 		],
 		[
-			'name' => 'Large',
+			'name' => 'Groß',
 			'shortName' => 'L',
 			'size' => 18,
 			'slug' => 'l',
 		],
 		[
-			'name' => 'Extra large',
+			'name' => 'Extra groß',
 			'shortName' => 'XL',
 			'size' => 22,
 			'slug' => 'xl',
